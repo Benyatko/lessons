39b35a0b6d1267a7a5fdce27e50e4a9a7e10ce95
Benyatko <botsko.andrii@gmail.com>
2025-05-23 15:50:27 +0300

    01

--- lesson_16/js/script.js
+++ lesson_16/js/script.js
@@ -3,6 +3,6 @@ document.addEventListener('click', documentActions)
 function documentActions(e) {
 	const targetElement = e.target 
 	if(targetElement.closest('.header__icon-menu')) {
-		document.body.classList.toggle('.menu-open')
+		document.body.classList.toggle('menu-open')
 	}
 }
